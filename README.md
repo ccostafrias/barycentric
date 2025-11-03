5ee8d122274939b5a9d71cd66d7e28111342528b
Caique Costa <ccostafrias@gmail.com>
2025-11-03 11:13:57 -0300

    feat: add all the instructions in the READ.md file
    Furthermore:
    - Modify the Image.fs to let it more readable;
    - Add the original file - Backup.fs - to the tests folder.

--- README.md
+++ README.md
@@ -1,2 +1,124 @@
-# barycentric
- Repositório destinado a guardar os arquivos do EP da matéria de MAVGA
+# Barycentric Renderer
+
+Esse repositório visa incluir os arquivo bases e instruir àqueles que pretendem realizar o EP de **MVGA**, ministrado pelo professor Helton Hideraldo Biscaro na **EACH USP**. 
+
+Desenvolvido em **OpenGL**, fazemos uso de computação gráfica para, no fim, renderizar um lindo e belo **triângulo** na tela!
+
+Tal projeto foi pensado para compilar e rodar facilmente em qualquer sistema **Linux** com suporte a OpenGL 3.3 ou superior. Caso esteja usando um sistema operacional diferente, como o **Windows**, minha dica é muito simples: *mude de sistema*!
+
+---
+
+## Como configurar
+
+### 1. Clonar o repositório
+
+Clone o projeto com seguinte comando:
+
+```bash
+git clone https://github.com/ccostafrias/barycentric.git
+cd barycentric
+```
+
+---
+
+### 2. Instalar dependências
+
+Agora, é necessário instalar alguns pacotes, como **OpenGL**, **GLFW**, **GLEW** e outras bibliotecas auxiliares:
+
+```bash
+sudo apt-get install g++ cmake git
+sudo apt-get install libsoil-dev libglm-dev libassimp-dev libglew-dev libglfw3-dev libxinerama-dev libxcursor-dev libxi-dev libfreetype-dev libgl1-mesa-dev xorg-dev
+```
+
+Esses pacotes garantem que tudo irá renderizar perfeitamente.
+
+---
+
+### 3. Compilar o projeto
+
+Para rodar o projeto, iremos rodar no terminal uma série de comandos, que visam:
+- limpar qualquer build anterior;
+- criar uma nova pasta build;
+- compilar e gerar um arquivo binário;
+- rodar o arquivo gerado.
+
+```bash
+rm -rf build
+mkdir build && cd build
+cmake ..
+cmake --build .
+cd ..
+./build/Barycentric
+```
+
+---
+
+## Objetivo
+
+Agora com o projeto configurado em mãos, o que fazer? O objetivo aqui é gerar um **triângulo** na tela utilizando **coordenadas baricêntricas** e fazê-lo reagir a estímulos do **mouse**, como mudar de cor, piscar, alterar o fundo, aplicar brilho, etc. — vai da sua imaginação! Para tal finalidade, abra o arquivo `Image.fs`, na pasta `Shaders`, e venha compreender mais um pouco da sua estrutura.
+
+---
+
+### 1. Vértices do triângulo
+
+Na função main, estão definidos os três vértices do triângulo (cuja origem é o **centro** da tela):
+
+```cpp
+vec2 a = vec2( 0.73,  0.75);
+vec2 b = vec2(-0.85,  0.15);
+vec2 c = vec2( 0.25, -0.75);
+```
+
+Sinta-se livre para modificá-los!
+
+---
+
+### 2. Cálculo das coordenadas baricêntricas
+
+A função `bary()` (ainda **NÃO** implementada) calcula as coordenadas baricêntricas de um ponto em relação aos vértices `a, b, c`.
+
+Essas coordenadas, resumidamente, indicam o quanto o ponto "pertence" a cada vértice, sendo que a **soma** dos componentes desse vetor **DEVE** ser igual a **um** (na dúvida, vale pesquisar e se aprofudnar um pouquinho mais).
+
+```cpp
+vec3 bary(in vec3 a, in vec3 b, in vec3 c, in vec3 p) {
+    return vec3(0.33, 0.33, 0.33); // exemplo de possível retorno
+}
+```
+
+---
+
+### 3. Testando pontos
+
+A função `test()` usa `bary()` para determinar se um ponto está dentro ou fora do triângulo — se todas coordenas baricêntricas forem positivas, está **dentro** e retorna `true`; se houver uma negativa, está **fora** e retorna `false`:
+
+```cpp
+bool test(in vec2 a, in vec2 b, in vec2 c, in vec2 p, inout vec3 barycoords);
+```
+
+Além disso, a função armazena as coordenadas baricêntricas em `barycoords` — um ponteiro (até aqui eles nos perseguem)!.
+
+---
+
+### 4. Renderização na tela
+
+Aqui precisamos entender um breve conceito: a função `main()` passa por cada pixel da tela e no final retornamos sua **cor** no formato **rgba**: red, green, blue, alpha (transparência).
+
+```cpp
+FragColor = vec4(color, 1);
+```
+
+Portanto, seu objetivo é implementar a lógica da função `bary()`, fazer uma série de testes lógicos na `main()` para decidir qual será a cor final do pixel, modificando `color`! 
+
+Para deixar o efeito final mais interessante, há uma série de funções adicionais (disponibilizadas pelo professor) das quais você pode fazer uso, além da variável `iTime` que possibilita fazer animações (o céu é o limite!).
+
+---
+
+## Conclusão
+
+Obrigado a todos que leram até aqui! Minha intenção nada mais é que ajudar outros colegas que estão (ou estiveram) tão perdidos quanto eu.
+
+Se houver qualquer equívoco, sinta-se livre para mandar um `pull-request` — qualquer contribuição é bem-vinda!
+
+Além disso, o shader original foi deixado na pasta `tests`, caso queira dar uma olhada e comparar.
+
+Enfim, boa sorte a todos e bons experimentos!
--- Shaders/Image.fs
+++ Shaders/Image.fs
@@ -12,34 +12,22 @@ float df_circ(in vec2 p, in vec2 c, in float r) {
     return abs(r - length(p - c));
 }
 
-// Find the intersection of "p" onto "ab".
+// acha intersecção de "p" na reta "ab"
 vec2 intersect (vec2 p, vec2 a, vec2 b) {
-    // Calculate the unit vector from "a" to "b".
     vec2 ba = normalize(b - a);
 
-    // Calculate the intersection of p onto "ab" by
-    // calculating the dot product between the unit vector
-    // "ba" and the direction vector from "a" to "p", then
-    // this value is multiplied by the unit vector "ab"
-    // fired from the point "a".
     return a + ba * dot(ba, p - a);
 }
 
-// Visual line for debugging purposes.
+// verifica se um ponto "p" está suficientemente perto da reta "ab"
 bool line (vec2 p, vec2 a, vec2 b) {
-    // Direction from a to b.
     vec2 ab = normalize(b - a);
-
-    // Direction from a to the pixel.
     vec2 ap = p - a;
 
-    // Find the intersection of the pixel on to vector
-    // from a to b, calculate the distance between the
-    // pixel and the intersection point, then compare
-    // that distance to the line width.
     return length((a + ab * dot(ab, ap)) - p) < 0.0025;
 }
 
+// distância de um ponto "p" até a reta "ab"
 float df_line(in vec2 p, in vec2 a, in vec2 b) {
     vec2 pa = p - a, ba = b - a;
     float h = clamp(dot(pa,ba) / dot(ba,ba), 0., 1.);
@@ -47,27 +35,12 @@ float df_line(in vec2 p, in vec2 a, in vec2 b) {
     return length(pa - ba * h);
 }
 
-float sharpen(in float d, in float w) {
-    float e = 1.0 / min(iResolution.y , iResolution.x);
-    return 1.0 - smoothstep(-e, e, d - w);
-}
-
-float df_bounds(in vec2 uv, in vec2 p, in vec2 a, in vec2 b, in vec2 c, in vec3 barycoords) {
-    float cp = 0.0;
-    float c0 = sharpen(df_circ(uv, p, (0.03 + cos(15.*iTime) *.01)), EPS * 1.);
-
-    return cp;
-}
-
-float dist_01(vec2 p, float r) {
-    float d = length(p);
-    return smoothstep(r, r + 0.01, d);
-}
-
+// calcula as coordenadas baricêntricas
 vec3 bary(in vec3 a, in vec3 b, in vec3 c, in vec3 p) {
     return vec3(0.333);
 }
 
+// testa se um ponto está dentro do triangulo ABC
 bool test(in vec2 a, in vec2 b, in vec2 c, in vec2 p, inout vec3 barycoords) {
     barycoords = bary(vec3(a.x, 0.0, a.y),
                       vec3(b.x, 0.0, b.y),
@@ -79,7 +52,7 @@ bool test(in vec2 a, in vec2 b, in vec2 c, in vec2 p, inout vec3 barycoords) {
 
 void main() {
     float ar = iResolution.x / iResolution.y;
-    vec2 mouse = (iMouse.xy / iResolution.xy * 2.0 - 1.0) * vec2(ar, 1.0) * vec2(1.0, -1.0);
+    vec2 mouse = (iMouse.xy / iResolution.xy * 2.0 - 1.0) * vec2(ar, 1.0) * vec2(1.0, -1.0); // retire esse último vetor caso esteja espelhado
     vec2 pixel = (gl_FragCoord.xy / iResolution.xy * 2.0 - 1.0) * vec2(ar, 1.0);
 
     vec2 a = vec2( 0.73,  0.75);
@@ -87,34 +60,24 @@ void main() {
     vec2 c = vec2( 0.25, -0.75);
 
     vec3 barycoords;
+    vec3 finalColor = vec3(0.0);
 
-    bool mouseInside = test(a, b, c, mouse, barycoords);
-    bool pixelInside = test(a, b, c, pixel, barycoords);
-
-    vec3 color = vec3(0.0);
     bool isDebug = true;
     
     if (isDebug) {
-        // linhas para delimitar o triangulo
-        if (line(pixel, a, b)) color = vec3(1.0, 1.0, 0.0);
-        if (line(pixel, b, c)) color = vec3(1.0, 0.0, 1.0);
-        if (line(pixel, c, a)) color = vec3(0.0, 1.0, 1.0);
+        // linhas para delimitar o triângulo
+        if (line(pixel, a, b)) finalColor = vec3(1.0, 1.0, 0.0);
+        if (line(pixel, b, c)) finalColor = vec3(1.0, 0.0, 1.0);
+        if (line(pixel, c, a)) finalColor = vec3(0.0, 1.0, 1.0);
 
-        if (df_circ(pixel, a, EPS) < 0.5*EPS) color = vec3(0.0, 1.0, 0.0);
-        if (df_circ(pixel, b, EPS) < 0.5*EPS) color = vec3(1.0, 0.0, 0.0);
-        if (df_circ(pixel, c, EPS) < 0.5*EPS) color = vec3(0.0, 0.0, 1.0);
-    }
-
-    if (pixelInside) {
-        if (mouseInside) {
-            // o que será, com os pixels do triangulo, quando o mouse estiver dentro
-        } else {
-            // o que será feito dentro do triângulo
-        }
+        // discos nos vértices
+        if (df_circ(pixel, a, EPS) < 0.5*EPS) finalColor = vec3(0.0, 1.0, 0.0);
+        if (df_circ(pixel, b, EPS) < 0.5*EPS) finalColor = vec3(1.0, 0.0, 0.0);
+        if (df_circ(pixel, c, EPS) < 0.5*EPS) finalColor = vec3(0.0, 0.0, 1.0);
     }
 
     // disco na posição do cursor
-    if (df_circ(mouse, pixel, EPS)< 0.5*EPS) color = vec3(1.0, 1.0, 1.0);
+    if (df_circ(mouse, pixel, EPS)< 0.5*EPS) finalColor = vec3(1.0, 1.0, 1.0);
 
-    FragColor = vec4(color, 1);
+    FragColor = vec4(finalColor, 1);
 }
